be24961ec97d3cd1fe65f50de2558dce3c506fcb
William Casarin <jb55@jb55.com>
2020-11-04 11:27:33 -0800

    quiver: fix luks config

--- nix-config/machines/quiver/hardware-configuration.nix
+++ nix-config/machines/quiver/hardware-configuration.nix
@@ -14,17 +14,17 @@
   boot.extraModulePackages = [ ];
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev"; 
-  boot.loader.grub.efiSupport = true; 
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices = [
-   { name = "root";
-     device = "/dev/disk/by-uuid/ddb70a55-f123-461d-a4c1-a42a393b61fa";
-     preLVM = false;
-     allowDiscards = true;
-   }
-  ];
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-uuid/ddb70a55-f123-461d-a4c1-a42a393b61fa";
+      preLVM = false;
+      allowDiscards = true;
+    };
+  };
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/a6670d72-9bdf-4c62-b397-d35c8c1356ef";
