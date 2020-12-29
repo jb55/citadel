d6173dc79a2ecb8ffab8cbac530a9397cf272ed1
William Casarin <jb55@jb55.com>
2020-12-29 10:31:01 -0800

    monad/hardware: remove cruft

--- nix-config/machines/monad/hardware/default.nix
+++ nix-config/machines/monad/hardware/default.nix
@@ -1,14 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  # fileSystems."/" =
-  #   { device = "/dev/disk/by-uuid/62518649-0872-49e2-a269-34975e314c6a";
-  #     fsType = "ext4";
-  #   };
-
-  # fileSystems."/" =
-  #   { device = "/dev/nvme0n1p1";
-  #     fsType = "zfs";
-  #nixos-generate-config --root /mnt   };
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" "amdgpu" "vfio-pci" ];
   boot.initrd.preDeviceCommands = ''
@@ -38,28 +29,6 @@
     { device = "zbig";
       fsType = "zfs";
     };
-
-  #fileSystems."/vr" =
-  # { device = "/dev/disk/by-uuid/E234A89834A87169";
-  #   fsType = "ntfs";
-  # };
-
-  #fileSystems."/sand" =
-  #  { device = "/dev/disk/by-uuid/2ee709b8-7e83-470f-91bc-d0b0ba59b945";
-  #    fsType = "ext4";
-  #  };
-
-  # fileSystems."/home/jb55/shares/will-vm/projects" =
-  #   { device = "//192.168.86.199/Users/jb55/projects";
-  #     fsType = "cifs";
-  #     options = ["username=jb55" "password=notsecurepw" "gid=100" "uid=1000"];
-  #   };
-
-  #fileSystems."/home/jb55/.local/share/Steam/steamapps" =
-  #  { device = "/sand/data/SteamAppsLinux";
-  #    fsType = "none";
-  #    options = ["bind"];
-  #  };
 
   # swapDevices =
   #   [ { device = "/dev/disk/by-uuid/d4e4ae51-9179-439d-925b-8df42dd1bfc5"; }
